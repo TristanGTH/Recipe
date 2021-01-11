494221da235d0ddfce9cccff0b20b0579f05ff69
Tristan <gauthier.tristan@yahoo.fr>
2021-01-11 17:07:59 +0100

    inshallah ça marche

--- app/RecipeManager.php
+++ app/RecipeManager.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace App;
+use App\Models\Recipe;
+use App\Storage\MySqlDatabaseRecipeStorage;
+
+class RecipeManager{
+    protected $storage;
+
+    public function __construct(MySqlDatabaseRecipeStorage $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public function addRecipe(Recipe $recipe){
+        $this->storage->store($recipe);
+    }
+
+    public function getRecipe($id){
+        $this->storage->get($id);
+    }
+
+    public function updateRecip(Recipe $recipe){
+        $this->storage->update($recipe);
+    }
+
+    public function getRecipes(){
+        $this->storage->all();
+    }
+}
